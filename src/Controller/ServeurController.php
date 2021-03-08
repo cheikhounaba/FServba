39b6966029f48adff5676307640cbf0a6174e8f2
Cheikhouna Ba <cheikhouna.ba@univ-st-etienne.fr>
2021-03-08 20:35:03 +0100

    Création route login

--- src/Controller/ServeurController.php
+++ src/Controller/ServeurController.php
@@ -25,5 +25,31 @@ class ServeurController extends AbstractController
         return $this->render('serveur/form.html.twig', [
             'controller_name' => 'ServeurController',
         ]);
-    } 
+    }
+      /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, EntityManagerInterface $manager, SessionInterface $session):
+    //Récupération des infos de login
+    {
+    $exampleInputEmail1=$request->request->get("exampleInputEmail1");
+    $exampleInputPassword1=$request->request->get("exampleInputPassword1");
+    //Recherche de l'utilisateur dans la base de données
+    $utilisateur = manager-> getRepository(Utilisateur :: class)->findOneByEmail=>txt 
+    //Vérifie si le mot de passe est correct
+    if($Utilisateur==NULL){
+        return new Response ("Utilisateur".$exampleInputEmail1""$exampleInputPassword1.inconnu)
+        else{
+            return $this->render('serveur/index.html.twig'), [
+            'controller_name' => 'ServeurController',
+
+        ]);
+            }  
+
+        /**
+     * @Route("/login", name="login")
+     */
+           
+
+    }
 }
