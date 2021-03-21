7c666fa632ccc2c425ccd25c6dd524105065301c
Cheikhouna Ba <cheikhouna.ba@univ-st-etienne.fr>
2021-03-21 20:00:31 +0100

    sauvegrde

--- src/Controller/ServeurController.php
+++ src/Controller/ServeurController.php
@@ -26,42 +26,5 @@ class ServeurController extends AbstractController
             'controller_name' => 'ServeurController',
         ]);
     }
-      /**
-     * @Route("/login", name="login")
-     */
-    public function login(Request $request, EntityManagerInterface $manager, SessionInterface $session): Response
-    //Récupération des infos de login
-    {
-    $exampleInputEmail1=$request->request->get("exampleInputEmail1");
-    $exampleInputPassword1=$request->request->get("exampleInputPassword1");
-    //Recherche de l'utilisateur dans la base de données
-    $Response = manager->getRepository(utilisateurs :: class)->findOneBy(['exampleInputEmail1'=>$exampleInputEmail1, 'exampleInputPassword1'=> $exampleInputPassword1]);
-    //Vérifie si le mot de passe est correct
-    if($Response==NULL){
-        return new Response ("utilisateur".$exampleInputEmail1""$exampleInputPassword1.inconnu)
-        else{
-            return $this->render('serveur/index.html.twig'), [
-            'controller_name' => 'ServeurController',
-
-        ]);
-            }  
-        }
-    }    
-      /*
-     * @Route("/createUser", name="createUser")
-     */
-    public function createUser(Request $request, EntityManagerInterface $manager): Response
-    {
-        $User = new Utilisateur();
-        $User->setexampleInputEmail1($request->request->get('exampleInputEmail1'));
-        $User->setexampleInputPassword1($request->request->get('exampleInputPassword1'));
-        
-        $manager->persist($User);
-        $manager->flush();
-        return $this->redirectToRoute('login');
-        
-        ]);
-    }  
-
-    
+      
 }
